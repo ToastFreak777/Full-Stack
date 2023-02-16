b4bc532c756545ad50e80015763962d6017136c5
ToastFreak <gmcdonald604@gmail.com>
2023-02-16 00:49:22 -0500

    implementing cart

--- nextjs-ecommerce/components/Navbar.jsx
+++ nextjs-ecommerce/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from "@/components";
+
+import { useStateContext } from "@/context/StateContext";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p>
@@ -9,10 +14,15 @@ const Navbar = () => {
           TF Headphones
         </Link>
       </p>
-      <button type="button" className="cart-icon" onCLick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">0</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
