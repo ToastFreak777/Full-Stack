2fa45433415dc4303c696a684e155ce1419f2ad8
ToastFreak <gmcdonald604@gmail.com>
2023-03-06 18:12:51 -0500

    initial commit

---
+++ User Auth/dist/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+app.use([express.static("./public")]);
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
